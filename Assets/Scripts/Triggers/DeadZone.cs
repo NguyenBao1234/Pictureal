7c90e60ceb958829ccaa7bb80c3ddd555bfab77d
NguyenBao1234 <robertpamvn@gmail.com>
2025-09-27 00:06:00 +0700

    - Dead zone trigger box

---
+++ Assets/Scripts/Triggers/DeadZone.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadZone : MonoBehaviour
+{
+    [Tooltip("Thời gian chờ trước khi restart (giây)")]
+    public float restartDelay = 1f;
+    public Animator FlashAnimator;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FlashAnimator.SetTrigger("FlashIn");
+            Debug.Log("Player fall into DeadZone! Restart level after " + restartDelay + " s.");
+            Invoke(nameof(RestartLevel), restartDelay);
+        }
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}

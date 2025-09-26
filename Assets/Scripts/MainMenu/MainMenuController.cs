a0e8c5b487fc0d4770f2f4963052b3e74347f498
TuanHiep <tuanhiep318@gmail.com>
2025-09-26 22:29:48 +0700

    - Insert Menu Image - Display FPS, Time Start in Main menu

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -10,7 +11,42 @@ public class MainMenuController : MonoBehaviour
     public SettingsMenuController settingMenuPanel;
     public Animator ScreenAnimator;
     
+    private float deltaTime;
+    private float startTime;
+    
+    public TMP_Text fpsText;
+    public TMP_Text timerText;
+    
+    public int targetFPS = 60;
+    
 
+    void Start()
+    {
+        Application.targetFrameRate = targetFPS;
+        startTime = Time.time;
+        if (fpsText != null)
+            fpsText.text = targetFPS + " FPS";
+    }
+
+    void Update()
+    {
+        if (fpsText != null)
+        {
+            int fpsInt = Mathf.RoundToInt(1f / Time.deltaTime);
+            fpsText.text = targetFPS + " FPS";
+        }
+
+        
+        float elapsed = Time.time - startTime;
+
+        int hours = (int)(elapsed / 3600f);
+        int minutes = (int)((elapsed % 3600f) / 60f);
+        int seconds = (int)(elapsed % 60f);
+        int milliseconds = (int)((elapsed - Mathf.Floor(elapsed)) * 100f);
+        
+        timerText.text = $"{hours:00}:{minutes:00}:{seconds:00}:{milliseconds:00}";
+    }
+    
     public void OnNewGame()
     {
         StartCoroutine(NewGameSequence());
